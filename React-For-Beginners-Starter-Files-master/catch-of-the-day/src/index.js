8af6edc33b3f2e2bb25ac3f8c5f82d0f0741f466
CyberAmigo <czavaleta93@gmail.com>
2016-11-01 17:13:48 -0500

    Used Router

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
@@ -1,9 +1,27 @@
 // let's go!
 import React from 'react';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import {render} from 'react-dom';
 import './css/style.css';
 import App from './components/App'
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+
+const Root = () => {
+	return (
+		<BrowserRouter>
+		<div>
+			<Match exactly pattern='/' component={StorePicker} />
+			<Match  pattern='/store/:storeId'component={App}/>
+			<Miss component={NotFound}/>
+		</div>
+		</BrowserRouter>
+
+	)
+}
 
 
 
-render(<App/>, document.querySelector('#main'));
+
+render(<Root/>, document.querySelector('#main'));
