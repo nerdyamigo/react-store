78358d99b367ddf450ea65b0ed2766eed9773a87
czavaleta <czavaleta93@gmail.com>
2016-11-01 23:05:38 -0500

    Added Router

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { getFunName } from '../helpers'
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  goToStore(e) {
+  	//grab text from box
+  	// trnasition from / -> /store/:storeId
+  	e.preventDefault();
+  	console.log(this.storeInput.value);
+  	const storeId = this.storeInput.value;
+  	this.context.router.transitionTo(`/store/${storeId}`)
+  }
+
   render () {
  	return (
 
- 		<form className="store-selector">
+ 		<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
  			<h2>Please Enter A Store</h2>
- 			<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+ 			<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input)=>{this.storeInput = input}} />
  			<button type="submit">Visit Store!</button>
  		</form>
  	) 
   }
 }
 
-
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
+}
 export default StorePicker;
